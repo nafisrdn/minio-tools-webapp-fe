7c2eab631937b355d25182159965aad974c39327
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-11-09 15:40:12 +0700

    add toggle sidebar

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,17 +1,24 @@
 import { Box, Container } from '@mui/material';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import Sidebar from '../Sidebar';
 import Topbar from '../Topbar';
 
 function Layout(props) {
   const { children } = props;
 
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const handleMenuClick = () => {
+    setSidebarVisible((state) => !state);
+  };
+
   return (
     <Box display="flex" height="100%" width="100%">
-      <Sidebar />
+      {sidebarVisible && <Sidebar />}
       <Box flex={1} overflow="auto">
         <Container>
-          <Topbar />
+          <Topbar onMenuClick={handleMenuClick} />
           <Box component="main" marginBottom={5}>
             {children}
           </Box>
--- src/Components/Topbar/index.jsx
+++ src/Components/Topbar/index.jsx
@@ -1,14 +1,20 @@
-import { DarkMode, LightMode } from '@mui/icons-material';
+import { DarkMode, LightMode, Menu } from '@mui/icons-material';
 import { IconButton, useTheme, Box } from '@mui/material';
 import { useContext } from 'react';
 import { ColorModeContext } from '../../theme';
 
-function Topbar() {
+function Topbar(props) {
+  const { onMenuClick } = props;
+
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
 
   return (
-    <Box display="flex" justifyContent="flex-end" alignItems="center" width="100%" height={90}>
+    <Box display="flex" justifyContent="space-between" alignItems="center" width="100%" height={90}>
+      <IconButton onClick={onMenuClick}>
+        <Menu />
+      </IconButton>
+
       <IconButton onClick={colorMode.toggleColorMode}>
         {theme.palette.mode === 'dark' ? <LightMode /> : <DarkMode />}
       </IconButton>
