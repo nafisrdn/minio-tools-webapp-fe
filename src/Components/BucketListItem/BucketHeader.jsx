11e3c2642d6ff72d6377f40823e6314e22f1e37f
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-11-09 11:43:48 +0700

    Update split header divider to its component

--- src/Components/BucketListItem/BucketHeader.jsx
+++ src/Components/BucketListItem/BucketHeader.jsx
@@ -2,6 +2,17 @@ import { AccessTime } from '@mui/icons-material';
 import { Box, Typography, useTheme } from '@mui/material';
 import { tokens } from '../../theme';
 
+function HeaderDivider() {
+  return (
+    <Box
+      width="90%"
+      height={10}
+      bgcolor={color.greenAccent[400]}
+      sx={{ borderBottomRightRadius: 100 }}
+    />
+  );
+}
+
 function BucketName(props) {
   const { bucket } = props;
 
@@ -14,18 +25,14 @@ function BucketName(props) {
         <Typography variant="h3" marginBottom={0.5}>
           {bucket.name}
         </Typography>
+
         <Box display="flex" gap={1} sx={{ color: color.greenAccent[100] }}>
           <AccessTime />
           <Typography>{bucket.lastModified}</Typography>
         </Box>
       </Box>
 
-      <Box
-        width="90%"
-        height={10}
-        bgcolor={color.greenAccent[400]}
-        sx={{ borderBottomRightRadius: 100 }}
-      />
+      <HeaderDivider />
     </Box>
   );
 }
